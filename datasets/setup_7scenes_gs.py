fa70ff050de55a32d33122315cb316c4b81a3b4e
khaled <khaled@nod-labs.com>
2021-04-01 11:37:11 -0400

    doing setup 7scenes for gs

--- datasets/setup_7scenes_gs.py
+++ datasets/setup_7scenes_gs.py
@@ -2,7 +2,7 @@ import os
 
 # name of the folder where we download the original 7scenes dataset to
 # we restructure the dataset by creating symbolic links to that folder
-src_folder = '7scenes_source'
+src_folder = '7Scenes'
 focallength = 525.0
 
 def mkdir(directory):
@@ -14,22 +14,22 @@ def mkdir(directory):
 mkdir(src_folder)
 os.chdir(src_folder)
 
-for ds in ['chess', 'fire', 'heads', 'office', 'pumpkin', 'redkitchen', 'stairs']:
+# for ds in ['chess', 'fire', 'heads', 'office', 'pumpkin', 'redkitchen', 'stairs']:
+for ds in ['chess']:
 	
-	print("=== Downloading 7scenes Data:", ds, "===============================")
+	# print("=== Downloading 7scenes Data:", ds, "===============================")
 
-	os.system('wget http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/' + ds + '.zip')
-	os.system('unzip ' + ds + '.zip')
-	os.system('rm ' + ds + '.zip')
+	# os.system('wget http://download.microsoft.com/download/2/8/5/28564B23-0828-408F-8631-23B1EFF1DAC8/' + ds + '.zip')
+	# os.system('unzip ' + ds + '.zip')
+	# os.system('rm ' + ds + '.zip')
 	
-	sequences = os.listdir(ds)
+	# sequences = os.listdir(ds)
 
-	for file in sequences:
-		if file.endswith('.zip'):
-
-			print("Unpacking", file)
-			os.system('unzip ' + ds + '/' + file + ' -d ' + ds)
-			os.system('rm ' + ds + '/' + file)
+	# for file in sequences:
+	# 	if file.endswith('.zip'):
+	# 		print("Unpacking", file)
+	# 		os.system('unzip ' + ds + '/' + file + ' -d ' + ds)
+	# 		os.system('rm ' + ds + '/' + file)
 
 	print("Linking files...")
 
