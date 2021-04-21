09c04d068931a3c00cd965a42a479a63ddab364b
khaled <khaled@nod-labs.com>
2021-04-20 23:52:33 -0400

    fix

--- preprocess/setup_sevenscenes.py
+++ preprocess/setup_sevenscenes.py
@@ -29,27 +29,29 @@ def download_data(src_folder, ds, rm_zip=False):
         print("File \'" + path + '.zip' + "\' elready exists, skipping download.")
 
 
-def link_frames(target_folder, split_file, variant, size=None):
+def link_frames(root, name, split_file, size=None):
+    kind = split_file.strip().split('Split')[0].lower()
     # create subfolders
-    mkdir(target_folder + variant + '/rgb/')
-    mkdir(target_folder + variant + '/poses/')
-    mkdir(target_folder + variant + '/calibration/')
+    mkdir(os.path.join(root, name, kind, 'rgb'))
+    mkdir(os.path.join(root, name, kind, 'poses'))
+    mkdir(os.path.join(root, name, kind, 'calibration'))
 
     # read the split file
-    with open(ds + '/' + split_file, 'r') as f:
+    with open(os.path.join(root, 'raw', name, split_file), 'r') as f:
         split = f.readlines()
     # map sequences to folder names
     split = ['seq-' + s.strip()[8:].zfill(2) for s in split]
 
     for seq in split:
-        files = os.listdir(ds + '/' + seq)
+        files = os.listdir(os.path.join(root, 'raw', name, seq))
 
         # link images
         images = [f for f in files if f.endswith('color.png')]
         count = 0
         for img in images[0:size]:
-            os.system('ln -sf ' + src_folder + '/' + ds + '/' + seq + '/' + img + ' ' 
-                    + target_folder + variant + '/rgb/' + seq + '-' + img)
+            os.system('ln -sf ' \
+                    + os.path.join(root, 'raw', name, seq, img) + ' ' \
+                    + os.path.join(root, name, kind, 'rgb', seq + '-' + img))
             count = count + 1
         print("Linked {:d} images.".format(count))
 
@@ -57,37 +59,55 @@ def link_frames(target_folder, split_file, variant, size=None):
         poses = [f for f in files if f.endswith('pose.txt')]
         count = 0
         for pose in poses[0:size]:
-            os.system('ln -sf ' + src_folder + '/' + ds + '/' + seq + '/' + pose + ' ' 
-                    + target_folder + variant + '/poses/' + seq + '-' + pose)
+            os.system('ln -sf ' \
+                    + os.path.join(root, 'raw', name, seq, pose) + ' ' \
+                    + os.path.join(root, name, kind, 'poses', seq + '-' + pose))
             count = count + 1
         print("Linked {:d} folders.".format(count))
 
         # create calibration files
         count = 0 
         for i in range(len(images[0:size])):
-            with open(target_folder+variant + '/calibration/{0:s}-frame-{1:s}.calibration.txt'
-                      .format(seq, str(i).zfill(6)), 'w') as f:
-                f.write(str(focallength))
+            fn = '{0:s}-frame-{1:s}.calibration.txt'.format(seq, str(i).zfill(6))
+            with open(os.path.join(root, name, kind, 'calibration', fn), 'w') as g:
+                g.write(str(focallength))
                 count = count + 1
         print("Written {:d} calibration files.".format(count))
+
+
+def make_frame_lists(target_folder, **kwargs):
+    """
+    This function makes two files in the data folder. One file contains a list of routes to all
+    train images (with poses etc.) and the other file contains a list of of routes to all test
+    imahes (with poses). We will use this from now on because the `link_frames()` function is not
+    very space efficient.
+    """
+    prefix = kwargs['prefix'] 
+    fname = prefix + '.txt'
+    path = os.path.join(target_folder, fname)
+    print(path)
+    fp = open(path)
+
+
 
 
 if __name__ == "__main__":
 
     # name of the folder where we download the original 7scenes dataset to
     # we restructure the dataset by creating symbolic links to that folder
-    data_root = os.environ['DATA_HOME'] 
-    src_folder = data_root + '/sevenscenes/raw'
+    data_home = os.environ['DATA_HOME']
+    root = os.path.join(data_home, 'sevenscenes')
+    raw_path = os.path.join(root, 'raw')
     focallength = 525.0
 
     # download the original 7 scenes dataset for poses and images
-    mkdir(src_folder)
-    os.chdir(src_folder)
+    mkdir(raw_path)
+    os.chdir(raw_path)
 
     # for ds in ['chess', 'fire', 'heads', 'office', 'pumpkin', 'redkitchen', 'stairs']:
-    for ds in ['chess']:
-        download_data(src_folder, ds)
-        target_folder = data_root + '/sevenscenes/' + ds + '/'
-        print("Linking files in ... " + target_folder)
-        link_frames(target_folder, 'TrainSplit.txt', 'train')
-        link_frames(target_folder, 'TestSplit.txt', 'test')
+    for name in ['chess']:
+        download_data(raw_path, name)
+        # make_frame_lists(os.path.join(data_root, 'sevenscenes'), prefix='{0:s}-train'.format(ds))
+        print("Linking files in ... " + root)
+        link_frames(root, name, 'TrainSplit.txt')
+        link_frames(root, name, 'TestSplit.txt')
