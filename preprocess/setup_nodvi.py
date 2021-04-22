89701f58e1ac63978809de449fb3fb4c9fb155b3
khaled <khaled@nod-labs.com>
2021-04-22 13:26:20 -0400

    each takes are different, therefore the matching of frames and poses needs to be done within individual take

--- preprocess/setup_nodvi.py
+++ preprocess/setup_nodvi.py
@@ -96,54 +96,54 @@ def parse_nodconfig(path):
                 config['cams']['cam0']['timeshift_cam_imu0'])
 
 
-def collect_images(root, takes):
+def collect_images(root):
     r"""Collect all the camera frames from the image folder.
 
-    Takes the root of the data folder and subfolders as different `takes`.
+    Takes the root of the data folder and subfolders as different `take`.
     Then it collates all the images in a single dict. The key is the timestamp
-    and the values are the path to image file.
+    and the values are the path to im`age file.
     """
+    image_path = os.path.join(root, 'nodvi/device/data/images0')
+    files = sorted(os.listdir(image_path))
+    
     images = {}
     duplicates = 0
-    for take in takes:
-        image_path = os.path.join(root, take, 'nodvi/device/data/images0')
-        files = sorted(os.listdir(image_path))
-        for file_name in files:
-            ts = int(file_name.split('.')[0])
-            if ts not in images:
-                images[ts] = os.path.join(image_path, file_name)
-            else:
-                duplicates = duplicates + 1
+    for file_name in files:
+        ts = int(file_name.split('.')[0])
+        if ts not in images:
+            images[ts] = os.path.join(image_path, file_name)
+        else:
+            duplicates = duplicates + 1
     if duplicates > 0:
         warnings.warn("Found {0:d} different images with the same timestamp. ".format(duplicates)
                       + "Please realign the timestamps with images or collect data again.")
     return images
 
 
-def collect_poses(root, takes):
+def collect_poses(root):
     r"""Collect all the ground-truth camera poses from the `groundtruth` folder.
 
     This function collects all the ground-truth camera poses and returns a dict.
     The keys are the timestamps of each pose and the values are camera position in
     3D and orientation in quarternion.
     """
+    config_path = os.path.join(root, 'nodvi/device/nodconfig.yaml')
+    intrinsics, shift = parse_nodconfig(config_path)
+    focal_length = intrinsics[0]
+    pose_path = os.path.join(root, 'nodvi/groundtruth/data.csv')
+    
     poses = {}
     duplicates = 0
-    for take in takes:
-        config_path = os.path.join(root, take, 'nodvi/device/nodconfig.yaml')
-        intrinsics, shift = parse_nodconfig(config_path)
-        focal_length = intrinsics[0]
-        pose_path = os.path.join(root, take, 'nodvi/groundtruth/data.csv')
-        with open(pose_path, 'r') as fp:
-            for line in fp.readlines()[1:]:
-                vals = line.strip().split(',')
-                ts, pose = int(vals[0]) + int(shift), [float(v)
-                                                       for v in vals[1:]]
-                pose.append(focal_length)
-                if ts not in poses:
-                    poses[ts] = pose
-                else:
-                    duplicates = duplicates + 1
+    with open(pose_path, 'r') as fp:
+        for line in fp.readlines()[1:]:
+            vals = line.strip().split(',')
+            ts, pose = int(vals[0]) + int(shift), [float(v)
+                                                   for v in vals[1:]]
+            pose.append(focal_length)
+            if ts not in poses:
+                poses[ts] = pose
+            else:
+                duplicates = duplicates + 1
     if duplicates > 0:
         warnings.warn("Found {0:d} different poses with the same timestamp. ".format(duplicates)
                       + "Please realign the timestamps with images or collect data again.")
@@ -186,26 +186,24 @@ def build_image_to_pose_map(images, poses, delta=0):
     return image_to_pose_map, mean_delta
 
 
-def search_best_delta():
+def search_best_delta(take):
     r"""Find the best delta. 
         Search for the best delta so that the mean deviation from the image timestamps
         and pose timestamps are minimized.
     """
-    data_home = os.environ['DATA_HOME']
     root = os.path.join(data_home, 'recordvi')
-    takes = ['recordvi-4-02-000', 'recordvi-4-02-003', 'recordvi-4-02-004']
     train_perc = 0.75
 
     # collect images
-    images = collect_images(root, takes)
+    images = collect_images(os.path.join(root, take))
     print("Found {0:d} camera frames.".format(len(images)))
     # collect ground truth poses
-    poses = collect_poses(root, takes)
+    poses = collect_poses(os.path.join(root, take))
     print("Found {0:d} camera poses.".format(len(poses)))
-
+    # find the best delta to minimize mismatch
     fp = open('delta.txt', 'w')
     min_mean_dev = [0, float('inf')]
-    for delta in range(2078000, 2080000, 10):
+    for delta in range(1, 3000000, 1000):
         # build the image timestamp to pose timestamp map
         itpmap, mean_dev = build_image_to_pose_map(images, poses, delta)
         print(delta, mean_dev)
@@ -238,43 +236,44 @@ if __name__ == "__main__":
     #               - data_no_header.csv (data.csv with no header)
     root = os.path.join(data_home, 'recordvi')
     takes = ['recordvi-4-02-000', 'recordvi-4-02-003', 'recordvi-4-02-004']
+    # these deltas were found using the search_best_delta() function
+    deltas = [2068001, 2072001, 2059001]
     train_perc = 0.75
 
-    # collect images
-    images = collect_images(root, takes)
-    print("Found {0:d} camera frames.".format(len(images)))
-    # collect ground truth poses
-    poses = collect_poses(root, takes)
-    print("Found {0:d} camera poses.".format(len(poses)))
-
-    min_mean_dev = [0, float('inf')]
-    # build the image timestamp to pose timestamp map
-    # delta=2078980 was found using the search_best_delta() function
-    itpmap, mean_dev = build_image_to_pose_map(images, poses, delta=2078980)
-    # print(itpmap)
-    its = list(itpmap.keys())
-    random.shuffle(its)
-    train_count = int(len(its) * train_perc)
+    data = []
+    for i in range(len(takes)):
+        path = os.path.join(root, takes[i])
+        # collect images
+        images = collect_images(path)
+        print("Found {0:d} camera frames in {1:s}.".format(len(images), path))
+        # collect ground truth poses
+        poses = collect_poses(path)
+        print("Found {0:d} camera poses in {1:s}.".format(len(poses), path))
+        # build the image timestamp to pose timestamp map
+        img_to_pose, mean_dev = build_image_to_pose_map(images, poses, delta=deltas[i])
+        for k,v in img_to_pose.items():
+            data.append([images[k],poses[v]])
+   
+    # shuffle the consolidated data
+    random.shuffle(data)
+    train_count = int(len(data) * train_perc)
 
     # makes the train data file mapping
     train_map_path = os.path.join(root, "train-map.csv")
     print("Writing training data mapping in {0:s}.".format(train_map_path))
     with open(train_map_path, 'w') as fp:
         for i in range(train_count):
-            img_key, pose_key = its[i], itpmap[its[i]]
-            line = images[img_key] + ',' + \
-                ','.join([str(v) for v in poses[pose_key]]) + '\n'
+            line = data[i][0] + ',' + ','.join([str(v) for v in data[i][1]]) + '\n'
             fp.write(line)
 
     # makes the test data file mapping
     test_map_path = os.path.join(root, "test-map.csv")
     print("Writing testing data mapping in {0:s}.".format(test_map_path))
     with open(test_map_path, 'w') as fp:
-        for i in range(train_count, len(its)):
-            img_key, pose_key = its[i], itpmap[its[i]]
-            line = images[img_key] + ',' + \
-                ','.join([str(v) for v in poses[pose_key]]) + '\n'
+        for i in range(train_count, len(data)):
+            line = data[i][0] + ',' + ','.join([str(v) for v in data[i][1]]) + '\n'
             fp.write(line)
+    
     print("Done.")
 
-    # search_best_delta()
+    # search_best_delta(takes[2])
