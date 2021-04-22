d57e2732a52d7c87851fd5c3a9b2145198176eb2
khaled <khaled@nod-labs.com>
2021-04-22 01:34:36 -0400

    fix

--- plot.sh
+++ plot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 lastinit=$(ls -t | grep log_init | head -n 1)
-if [ -n $lastinit ]; then
-    echo "here1"
-gnuplot -persist <<-EOFMarker
+[[ ! -z $lastinit ]] && gnuplot -persist <<-EOFMarker
     set term png
     set output "convergence-init.png"
     set xlabel "Iterations"
@@ -12,12 +10,9 @@ gnuplot -persist <<-EOFMarker
     plot "$lastinit" using 1:2 every 4000 with lines
     set term wxt
 EOFMarker
-fi
 
 laste2e=$(ls -t | grep log_e2e | head -n 1)
-if [ -n $laste2e ]; then
-    echo "here2"
-gnuplot -persist <<-EOFMarker
+[[ ! -z $laste2e ]] && gnuplot -persist <<-EOFMarker
     set term png
     set output "convergence-e2e.png"
     set xlabel "Iterations"
@@ -26,4 +21,3 @@ gnuplot -persist <<-EOFMarker
     plot "$laste2e" using 1:2 every 400 with lines
     set term wxt
 EOFMarker
-fi
