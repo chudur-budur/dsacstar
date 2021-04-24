17c5738df87c21a2b26ea302c21fcf2814220bcc
khaled <khaled@nod-labs.com>
2021-04-24 19:23:31 -0400

    now JellyfishDataset returns timestamps

--- test.py
+++ test.py
@@ -165,7 +165,7 @@ with torch.no_grad():
 
         print(timestamp, file_, r_err, t_err, q_w, q_xyz, t)
         pose_log.write("{0:s}\t{1:s}".format(timestamp[0], file_) 
-                + "\t{2:.6f}\t{3:.6f}\t{4:.6f}\t{5:.6f}".fromat(q_w, q_xyz[0][0], q_xyz[1][0], q_xyz[2][0]) 
+                + "\t{2:.6f}\t{3:.6f}\t{4:.6f}\t{5:.6f}".format(q_w, q_xyz[0][0], q_xyz[1][0], q_xyz[2][0]) 
                 + "\t{6:.6f}\t{7:.6f}\t{8:.6f}".format(t[0].item(), t[1].tiem(), t[2].item())
                 + "\t{9:.6f}\t{10:.6f}\n".format(r_err, t_err))
 
