1a14419e434d076935228d09e5ac67f0ff21473e
khaled <khaled@nod-labs.com>
2021-04-26 05:19:49 -0400

    plot iterations and epoch separately

--- plot.sh
+++ plot.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
-n=$1
-if [[ -z $n ]]; then
-    n=25
-fi
-
-m=$2
-if [[ -z $m ]]; then
-    m=25
-fi
-
-lastinit=$(ls -t | grep log_init | head -n 1)
+lastinit=$(ls -t | grep log_init_iter | head -n 1)
 if [[ ! -z $lastinit ]]; then
     gnuplot <<- EOF
         set term png
-        set output "convergence-init.png"
+        set output "conv-init-iter.png"
         set xlabel "Iterations"
         set ylabel "Loss"
         unset key
-        plot "$lastinit" using 1:2 every $n with lines
+        plot "$lastinit" using 1:2 with lines
         set term wxt
 EOF
 fi
 
-laste2e=$(ls -t | grep log_e2e | head -n 1)
-if [[ ! -z $laste2e ]]; then
+lastinit=$(ls -t | grep log_init_epoch | head -n 1)
+if [[ ! -z $lastinit ]]; then
     gnuplot <<- EOF
         set term png
-        set output "convergence-e2e.png"
+        set output "conv-init-epoch.png"
         set xlabel "Iterations"
         set ylabel "Loss"
         unset key
-        plot "$laste2e" using 1:2 every $m with lines
+        plot "$lastinit" using 1:2 with lines
+        set term wxt
+EOF
+fi
+
+laste2e=$(ls -t | grep log_e2e_iter | head -n 1)
+if [[ ! -z $laste2e ]]; then
+    gnuplot <<- EOF
+        set term png
+        set output "conv-e2e-iter.png"
+        set xlabel "Iterations"
+        set ylabel "Loss"
+        unset key
+        plot "$laste2e" using 1:2 with lines
+        set term wxt
+EOF
+fi
+
+laste2e=$(ls -t | grep log_e2e_epoch | head -n 1)
+if [[ ! -z $laste2e ]]; then
+    gnuplot <<- EOF
+        set term png
+        set output "conv-e2e-epoch.png"
+        set xlabel "Iterations"
+        set ylabel "Loss"
+        unset key
+        plot "$laste2e" using 1:2 with lines
         set term wxt
 EOF
 fi
