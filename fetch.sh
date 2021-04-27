6af2fec7abc5af2c00c8134482076e279c47df95
khaled <khaled@nod-labs.com>
2021-04-26 20:49:19 -0400

    sanity check

--- fetch.sh
+++ fetch.sh
@@ -2,7 +2,18 @@
 # Script to generate plots and downloading.
 # Usage: $ ./fetch.sh
 
-echo "Generating plots ..."
-gcloud compute ssh n1s8-2nvtk80 --command "cd dsacstar && ./plot.sh"
-echo "Downloading ..."
-gcloud compute scp n1s8-2nvtk80:~/dsacstar/*.png .
+probe=$(command -v gcloud)
+if [ -n $probe ]; then
+    echo "Generating plots ..."
+    gcloud compute ssh n1s8-2nvtk80 --command "cd dsacstar && ./plot.sh"
+    echo "Downloading ..."
+    gcloud compute scp n1s8-2nvtk80:~/dsacstar/*.png .
+fi
+
+probe=$(command -v xdg-open)
+if [ -n $probe ]; then
+    xdg-open conv-init-epoch.png &
+    xdg-open conv-init-epoch-pvsc.png &
+    xdg-open conv-init-iter.png &
+    xdg-open conv-init-iter-pvsc.png &
+fi
