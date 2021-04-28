1b44145fbc2957d0bcc886a4eb3cc930298ae34c
khaled <khaled@nod-labs.com>
2021-04-28 01:13:49 -0400

    fix fisheye and rescale

--- dataset.py
+++ dataset.py
@@ -127,8 +127,6 @@ class JellyfishDataset(Dataset):
 
     def __compute_pose_simple__(self, p, q):
         R = np.zeros((3, 3))
-        R[0:3, -1] = p
-
         R[0, 0] = 2 * (q[0] * q[0] + q[1] * q[1]) - 1
         R[0, 1] = 2 * (q[1] * q[2] - q[0] * q[3])
         R[0, 2] = 2 * (q[1] * q[3] + q[0] * q[2])
