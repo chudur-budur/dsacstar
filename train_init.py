8141d5c13d78c35e49c73e11097a68ff8c9bad62
khaled <khaled@nod-labs.com>
2021-04-28 05:36:39 -0400

    fix fisheye and rescale

--- train_init.py
+++ train_init.py
@@ -74,7 +74,7 @@ if not os.path.exists(model_root):
 trainset = JellyfishDataset(opt.scene, mode=min(
     opt.mode, 1), sparse=opt.sparse, augment=True)
 trainset_loader = torch.utils.data.DataLoader(
-    trainset, shuffle=True, num_workers=2)
+    trainset, shuffle=True, num_workers=10)
 
 print("Found {0:d} training images in {1:s}.".format(
     len(trainset), opt.scene))
@@ -165,14 +165,12 @@ for epoch in range(1, epochs+1):
     count = 0
     mean_loss = 0.0
     mean_num_valid_sc = 0.0
-    print("here1")
     for image, gt_pose, gt_coords, focal_length, _, _ in trainset_loader:
-        print("here2")
-        if sanity_check and count < 10 and epoch < 2:
-            home = os.environ['HOME']
-            path = os.path.join(home, 'tmp/{0:d}-unfished.png'.format(count))
-            print("Saving", path)
-            utils.save_image(image, path)
+        # if sanity_check and count < 10 and epoch < 2:
+        #     home = os.environ['HOME']
+        #     path = os.path.join(home, 'tmp/{0:d}-unfished.png'.format(count))
+        #     print("Saving", path)
+        #     utils.save_image(image, path)
 
         start_time = time.time()
 
