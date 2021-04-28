9319360b96373789ce545b2bd28de44a84598018
khaled <khaled@nod-labs.com>
2021-04-28 04:44:29 -0400

    fix fisheye and rescale

--- train_init.py
+++ train_init.py
@@ -167,6 +167,7 @@ for epoch in range(1, epochs+1):
     for image, gt_pose, gt_coords, focal_length, _, _ in trainset_loader:
 
         if count < 10 and epoch < 2:
+            print("saving", count)
             utils.save_image(image, '/tmp/{0:d}-unfished.png'.format(count))
 
         start_time = time.time()
