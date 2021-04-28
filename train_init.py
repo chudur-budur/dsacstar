898f6ae1f51411f9c32683da155ea6e0116ed46b
khaled <khaled@nod-labs.com>
2021-04-28 05:31:26 -0400

    fix fisheye and rescale

--- train_init.py
+++ train_init.py
@@ -74,7 +74,7 @@ if not os.path.exists(model_root):
 trainset = JellyfishDataset(opt.scene, mode=min(
     opt.mode, 1), sparse=opt.sparse, augment=True)
 trainset_loader = torch.utils.data.DataLoader(
-    trainset, shuffle=True, num_workers=6)
+    trainset, shuffle=True, num_workers=0)
 
 print("Found {0:d} training images in {1:s}.".format(
     len(trainset), opt.scene))
