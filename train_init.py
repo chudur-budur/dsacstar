ff1b2bf880e37a0beefb8d93b2d111f52166def2
khaled <khaled@nod-labs.com>
2021-04-28 05:29:17 -0400

    fix fisheye and rescale

--- train_init.py
+++ train_init.py
@@ -169,8 +169,8 @@ for epoch in range(1, epochs+1):
     for image, gt_pose, gt_coords, focal_length, _, _ in trainset_loader:
         print("here2")
         if sanity_check and count < 10 and epoch < 2:
-            # home = os.getenv('$HOME')
-            path = os.path.join('/home/khaled', 'tmp/{0:d}-unfished.png'.format(count))
+            home = os.environ['$HOME']
+            path = os.path.join(home, 'tmp/{0:d}-unfished.png'.format(count))
             print("Saving", path)
             utils.save_image(image, path)
 
