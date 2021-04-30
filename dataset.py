bb0643f6f3425e3dc886ca7f503fa488f7f7ebfb
khaled <khaled@nod-labs.com>
2021-04-30 02:21:08 -0400

    passing all image processing functions in lambdas for Compose

--- dataset.py
+++ dataset.py
@@ -23,7 +23,7 @@ homepath = os.environ['HOME']
 print(homepath)
 sys.path.append('/home/khaled/dsacstar/opecv_transforms_torchvision/cvtorchvision')
 print(sys.path)
-from cvtorchvision import cvtransforms
+import cvtransforms
 
 class JellyfishDataset(Dataset):
     """Camera localization dataset for Jellyfish SLAM.
