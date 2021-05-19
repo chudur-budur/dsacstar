d9cb6fd4a0714d557c512280d27a8610d3277970
khaled <khaled@nod-labs.com>
2021-05-19 08:16:35 -0400

    use image similarity difference matrix for tsne instead of the whole rgb values

--- analytics.py
+++ analytics.py
@@ -10,6 +10,7 @@ from sklearn import cluster
 import transforms as tr
 import cv2
 from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import normalized_root_mse as nrmse
 
 
 __all__ = ['load_raw', 'save_flat', 'load_flat']
@@ -104,7 +105,7 @@ def build_image_dist_matrix(M, dim=(96,54)):
     for i in range(n):
         for j in range(n):
             if j <= i:
-                D[i,j] = ssim(M[i].reshape(dim[1], dim[0]), M[j].reshape(dim[1], dim[0]))
+                D[i,j] = nrmse(M[i].reshape(dim[1], dim[0]), M[j].reshape(dim[1], dim[0]))
         print('Finished row i = {0:d}'.format(i))
     return D
 
