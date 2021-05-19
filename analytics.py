60f3c0c729650a416841007fb7d7654b6ab769e2
khaled <khaled@nod-labs.com>
2021-05-19 03:04:31 -0400

    loasing and saving

--- analytics.py
+++ analytics.py
@@ -3,16 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import datasets
 from sklearn.manifold import TSNE
-from torchvision import transforms
 import transforms as tr
-from skimage import io, color
 import cv2
 
-def make_pileline(img, came):
-    pass
+__all__ = ['load_raw', 'save_flat', 'load_flat']
 
-def load_data(map_file_path):
 
+def load_raw(map_file_path, n=float('inf')):
     data = {}
     fp = open(map_file_path, 'r')
     count = 0
@@ -24,24 +21,8 @@ def load_data(map_file_path):
         camera_intrinsics = np.array([float(v) for v in vals[8:12]]).astype(float)
         distortion_coeffs = np.array([float(v) for v in vals[12:-1]]).astype(float)
         
-        # if len(image.shape) < 3:
-        #     image = color.gray2rgb(image)
-
-        # pipeline = transforms.Compose([
-        #     transforms.Lambda(lambda img: tr.unfish(
-        #         img, camera_intrinsics, distortion_coeffs)),
-        #     transforms.Lambda(lambda img: tr.cambridgify(img)),
-        #     transforms.ToPILImage(),
-        #     transforms.Resize(16),
-        #     transforms.Grayscale(),
-        #     transforms.ColorJitter(brightness=0.1, contrast=0.1),
-        #     transforms.ToTensor()
-        #     ])
-        # image = pipeline(image)
-        
         image = cv2.imread(image_path, 0)
         image = tr.unfish(image, camera_intrinsics, distortion_coeffs)
-        image = tr.cambridgify(image)
         scale_perc = 0.05 # percent of original size
         w, h = int(image.shape[1] * scale_perc), int(image.shape[0] * scale_perc)
         image = cv2.resize(image, (w, h), interpolation = cv2.INTER_AREA)  
@@ -49,17 +30,46 @@ def load_data(map_file_path):
         data[ts] = [pose, image]
 
         count = count + 1
-        if count == 10:
+        if count > n:
             break
         if count % 100 == 0:
             print("Loaded {0:d} images and poses.".format(count))
 
     fp.close()
+    return data, (w,h)
+
+
+def save_flat(data, path):
+    fp = open(path, 'w')
+    for key in data.keys():
+        ts = key
+        pose = data[key][0]
+        image = data[key][1]
+        fp.write(key + ',' + ','.join([v for v in pose]) + ',' + ','.join([v for v in image] + '\n')
+    fp.close()
+
+
+def load_flat(path):
+    fp = open(path, 'r')
+    data = {}
+    for line in fp:
+        vals = line.strip().split(',')
+        key = vals[0]
+        pose = np.array([float(v) for v in vals[1:8]]).astype(float)
+        image = np.array([int(v) for v in vals[8:]]).astype(int)
+        data[key] = [pose, image]
+    fp.close()
     return data
 
+
 if __name__ == "__main__":
-    data = load_data("split-files/jellyfish-train-map.csv")
-    # print(data)
+    data, dim = load_raw("split-files/jellyfish-train-map.csv", n=10)
+    print(len(data))
+    keys = list(data.keys())
+    print(keys)
+    cv2.imwrite(data[keys[0]][1].reshape(dim), "test.png")
+    save_flat(data, "flat.csv")
+    data = load_flat("flat.csv")
 
     # digits = datasets.load_digits()
     # 
