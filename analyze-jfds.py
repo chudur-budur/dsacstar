a9e1263bfa4baeb06d7fad95d589fc9e8a053d46
khaled <khaled@nod-labs.com>
2021-05-19 01:40:54 -0400

    adding tsne data analysis

--- analyze-jfds.py
+++ analyze-jfds.py
@@ -15,10 +15,7 @@ def load_data(map_file_path):
     fp = open(map_file_path, 'r')
     for line in fp:
         vals = line.strip().split(',')
-        image_path = vals[0]
-
-        path,file_name = os.path.split(image_path)
-        ts = file_name.split('.')[0]
+        ts = os.path.split(vals[0])[1].split('.')[0] 
         camera_intrinsics = np.array([float(v) for v in vals[1:8]]).astype(float)
         distortion_coeffs = np.array([float(v) for v in vals[8:-1]]).astype(float)
 
@@ -29,7 +26,9 @@ def load_data(map_file_path):
             transforms.ToPILImage(),
             transforms.Resize(120),
             transforms.Grayscale(),
-            transforms.ColorJitter(brightness=0.1, contrast=0.1)])
+            transforms.ColorJitter(brightness=0.1, contrast=0.1),
+            transforms.ToTensor().numpy()
+            ])
 
         image = pipeline(image_path)
         data[ts] = [pose, image]
@@ -37,26 +36,26 @@ def load_data(map_file_path):
     return data
 
 if __name__ == "__main__":
-    # data = load_data("split-files/jellyfish-train-map.csv")
+    data = load_data("split-files/jellyfish-train-map.csv")
     # print(data)
 
-    digits = datasets.load_digits()
-    
-    print("len(digits):", len(digits), "type(digits):", type(digits))
-    print("len(digits.data[0]):", len(digits.data[0]), "digits.data[0]:", \
-            digits.data[0], "digits.target[0]:", digits.target[0])
-    
-    # Take the first 500 data points: it's hard to see 1500 points
-    X = digits.data[:500]
-    y = digits.target[:500]
+    # digits = datasets.load_digits()
+    # 
+    # print("len(digits):", len(digits), "type(digits):", type(digits))
+    # print("len(digits.data[0]):", len(digits.data[0]), "digits.data[0]:", \
+    #         digits.data[0], "digits.target[0]:", digits.target[0])
+    # 
+    # # Take the first 500 data points: it's hard to see 1500 points
+    # X = digits.data[:500]
+    # y = digits.target[:500]
 
-    tsne = TSNE(n_components=2, random_state=0)
-    X_2d = tsne.fit_transform(X)
-    target_ids = range(len(digits.target_names))
+    # tsne = TSNE(n_components=2, random_state=0)
+    # X_2d = tsne.fit_transform(X)
+    # target_ids = range(len(digits.target_names))
 
-    plt.figure(figsize=(6, 5))
-    colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
-    for i, c, label in zip(target_ids, colors, digits.target_names):
-        plt.scatter(X_2d[y == i, 0], X_2d[y == i, 1], c=c, label=label, s=1)
-    plt.legend()
-    plt.show()
+    # plt.figure(figsize=(6, 5))
+    # colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w', 'orange', 'purple'
+    # for i, c, label in zip(target_ids, colors, digits.target_names):
+    #     plt.scatter(X_2d[y == i, 0], X_2d[y == i, 1], c=c, label=label, s=5)
+    # plt.legend()
+    # plt.show()
