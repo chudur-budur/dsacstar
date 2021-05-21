5a0c88a75b86db02c3ed884d9f3739014098aad6
khaled <khaled@nod-labs.com>
2021-05-21 18:44:25 -0400

    parallel pairwise dist metric

--- analytics.py
+++ analytics.py
@@ -127,13 +127,13 @@ def build_image_dist_matrix(M, dim=(96,54), mode='normalized_root_mse'):
 if __name__ == "__main__":
     np.random.seed(123456)
 
-    # print("Loading raw image and pose data")
-    # data, dim = load_raw("split-files/jellyfish-train-map.csv", scale=0.1)
-    # print(len(data), dim)
-    # keys = list(data.keys())
-    # print(keys[0:5])
-    # cv2.imwrite("test.png", data[keys[0]][1].reshape(dim[1], dim[0]))
-    # save_flat(data, "flat.csv")
+    print("Loading raw image and pose data")
+    data, dim = load_raw("split-files/jellyfish-train-map.csv", scale=0.75)
+    print(len(data), dim)
+    keys = list(data.keys())
+    print(keys[0:5])
+    cv2.imwrite("test.png", data[keys[0]][1].reshape(dim[1], dim[0]))
+    save_flat(data, "flat.csv")
 
     print("Loading flattened image and pose data")
     data = load_flat("flat.csv")
